d26e838afe0f591886ec6ccc9b746d776bfd0f5d
SwagSek <lolinz666@gmail.com>
2024-03-21 13:40:12 +0200

    Update: Page class

--- src/model/Page.java
+++ src/model/Page.java
@@ -8,7 +8,8 @@ public class Page {
 	private String title;
 	private String description;
 	private ArrayList<User> followers = new ArrayList<User>();
-	private ArrayList<Post> postsInPage = new ArrayList<Post>();
+	private ArrayList<Post> publicPosts = new ArrayList<Post>();
+	private ArrayList<Post> privatePosts = new ArrayList<Post>();
 	
 	//Getters
 	public String getTitle() {
@@ -19,20 +20,45 @@ public class Page {
 		return description;
 	}
 	
+	public ArrayList<Post> getPublicPosts() {
+		return publicPosts;
+	}
+	
+	public ArrayList<Post> getPrivatePosts() {
+		return privatePosts;
+	}
+	
+	public ArrayList<User> getFollowers() {
+		return followers;
+	}
+	
 	//Setters
 	public void setTitle(String title) {
-		if (title != null && title.length() > 5 && title.length() < 20)
+		if (title != null && title.matches("[A-Za-z0-9 ]{5,20}"))
 			this.title = title;
 		else
 			this.title = "Undefined";
 	}
 	
 	public void setDescription(String description) {
-		if (description != null && description.length() > 3 && description.length() < 100)
+		if (description != null && description.length() > 10 && description.length() < 200)
 			this.description = description;
 		else
 			this.description = "Undefined";
 	}
 	
+	//Constructors
+	public Page() {
+		setTitle("Untitled");
+		setDescription("Untitled");
+	}
 	
+	public Page(String title, String description) {
+		setTitle(title);
+		setDescription(description);
+	}
+	
+	public String toString() {
+		return title + " (" + description + "): [" + (privatePosts.size() + publicPosts.size()) + " posts]";
+	}
 }
